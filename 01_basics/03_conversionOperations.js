001d40caca93682015673c9980ff91ed86268205
Rohit Halsana <110708807+RohitHalsana@users.noreply.github.com>
2024-11-05 12:28:39 +0000

    testing js code

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -32,3 +32,35 @@ console.log(typeof stringNumber);
 let value =3
 let negValue = -value
 console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Rohit"
+
+let str3 = str1+str2;
+
+console.log (str3);
+
+let str4=str1*str2;
+
+console.log(str4);
+
+console.log(1+2+"2");
+console.log("1"+2+2)
+
+console.log(+true)
+console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2
+
+gc=100
+gc++;
+console.log(gc);
+++gc;
+console.log(gc);
---
+++ 01_basics/04_comparisson.js
@@ -0,0 +1,15 @@
+console.log(2>3);
+console.log("2">1);
+// =====
+console.log("Confussion")
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// =====
+console.log("Strict comparission")
+console.log("2" === 2);
