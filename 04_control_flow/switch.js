a8e9f7990a946a1322c09eca041a3182e0423672
Rohit Halsana <110708807+RohitHalsana@users.noreply.github.com>
2024-11-20 13:45:00 +0000

    switch case condition

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,15 @@
+let n =5;
+switch(n){
+    case 1:
+        console.log("1");
+        break;
+    case 2:
+        console.log("2");
+        break;
+    case 3:
+        console.log("3");
+        break;
+    default:
+        console.log("Default");
+        break;
+}
