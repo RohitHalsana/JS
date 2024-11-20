063641331d4a9548ad687e3d539bb1ce62e55813
Rohit Halsana <110708807+RohitHalsana@users.noreply.github.com>
2024-11-20 13:44:41 +0000

    Learning control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,34 @@
+// if
+
+// if (condition){
+//     //true
+// }
+// //false
+
+const is= true
+if (true)
+    console.log(true);
+    
+const t= 50
+
+if (t<60)
+    console.log("less than 60");
+else
+    console.log("more than 60");
+console.log("EXECUTED");
+
+const b=600
+if (b>500) console.log(b),console.log(b+200);
+
+a=true
+b2=true
+c=false
+
+if (a && b2)
+    console.log(true);
+
+if(a || c)
+    console.log(true);
+    
+    
+    
