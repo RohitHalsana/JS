164720bced1b7f7944df990b37fb71b83ea9d703
Rohit Halsana <110708807+RohitHalsana@users.noreply.github.com>
2024-11-21 17:26:47 +0000

    while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,19 @@
+let index = 0
+while (index <= 10){
+    console.log(`Value of i = ${index}`);
+    index+=2;
+}
+
+let myArray = ['flash', 'Batman',  'superman']
+
+let arr = 0
+while (arr< myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+let s=1
+do {
+    console.log(s);
+    s++;
+}while(s<8);
