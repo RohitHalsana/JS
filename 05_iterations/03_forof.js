332e87b715f59f7197a4d4ddd83da55097e2c181
RohitHalsana <rohithalsana08@gmail.com>
2025-03-11 18:35:27 +0530

    learning for of loop

--- 05_iterations/03_forof.js
+++ 05_iterations/03_forof.js
@@ -1,5 +1,32 @@
 //for of
 
-["", "", ""]
-[{}, {}, {}]
+// ["", "", ""]
+// [{}, {}, {}]
 
+const greeting ="Hello World"
+for( const greet of greeting){
+    console.log(greet);
+}
+const map = new Map();
+map.set("IN","India")
+map.set("USA","United States of America");
+map.set("UK","United Kindom")
+
+console.log(map);
+
+for (const[key,value] of map){
+    console.log(key,value)
+}
+
+const myObject = {
+    game1: "NFS",
+    game2: "Spiderman"
+}
+// for (const[key,value] of myObject){
+//     console.log(key,value);
+// } this for of doesnt work for objects
+
+
+for (const [key, value] of Object.entries(myObject)) {
+    console.log(key, value);
+}
