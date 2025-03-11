0ba0600f8257b126dbbeb1f9c4240b32b38fec3a
RohitHalsana <rohithalsana08@gmail.com>
2025-03-11 18:39:48 +0530

    learning for Each llop

---
+++ 05_iterations/05_forEach.js
@@ -0,0 +1,31 @@
+// const code=[ "js", "java", "python", "c++"]
+
+// code.forEach(function (val){
+//     console.log(val);
+// })
+
+// code.forEach((item,index,arr)=>{
+ // console.log(item,index,arr);
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+
+// code.forEach(printme)
+
+// ===================================================================================
+const myCode=[
+    { 
+        languageName:"JavaScript",
+        fileName:"Js"
+    },
+    { 
+        languageName:"Java",
+        fileName:"Java"
+    },
+    { 
+        languageName:"Python",
+        fileName:"Py"
+    }
+]
